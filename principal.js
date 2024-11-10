955a27c8c44904d705a954958229f3424c7724bc
facchinetti.dev <facchinetti.dev@gmail.com>
2024-11-10 14:16:31 -0300

    Add principal.js

--- principal.js
+++ principal.js
@@ -3,6 +3,6 @@ var bannerAtual = 0;
 
 function trocaBanner() {
     bannerAtual = (bannerAtual + 1) % 2;
-    document.querySelector(’h2#mensagem’).textContent = banners[bannerAtual];
+    document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
 setInterval(trocaBanner, 2000);
